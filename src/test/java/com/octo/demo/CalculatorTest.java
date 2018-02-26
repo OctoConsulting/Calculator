f6a1e5abe429167fe0a9c427504b39d994c91ac9
nvishy <vishy_work@yahoo.com>
2018-02-25 22:11:21 -0500

    SON-15
    SON-15

--- src/test/java/com/octo/demo/CalculatorTest.java
+++ src/test/java/com/octo/demo/CalculatorTest.java
@@ -28,6 +28,7 @@ public class CalculatorTest {
 		System.out.println("This is exceuted after each Test ");
 		System.out.println("This is exceuted after each Test ");
 		System.out.println("This is exceuted after each Test ");
+		System.out.println("This is exceuted after each Test ");
 	}
 
 	@Test

74e100ca359916f14808ee4c7b7716ab0abc0ec4
nvishy <vishy_work@yahoo.com>
2018-02-28 12:57:06 -0500

    CALC-9
    CALC-9

--- src/test/java/com/octo/demo/CalculatorTest.java
+++ src/test/java/com/octo/demo/CalculatorTest.java
@@ -26,7 +26,7 @@ public class CalculatorTest {
 	public void afterEachTest() {
 		System.out.println("This is exceuted after each Test ");
 		System.out.println("This is exceuted after each Test ");
-		System.out.println("This is exceuted after each Test ")
+		System.out.println("This is exceuted after each Test ");
 		System.out.println("This is exceuted after each Test ");
 		System.out.println("This is exceuted after each Test ");
 		System.out.println("This is exceuted after each Test ");

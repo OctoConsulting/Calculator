ead0b73b6a789cebd97658803154f2e7848197ce
nvishy <vishy_work@yahoo.com>
2018-03-01 12:23:28 -0500

    CALC-12
    CALC-12

--- src/test/java/com/octo/demo/CalculatorTest.java
+++ src/test/java/com/octo/demo/CalculatorTest.java
@@ -31,7 +31,7 @@ public class CalculatorTest {
 		System.out.println("This is exceuted after each Test ");
 		System.out.println("This is exceuted after each Test ");
 		System.out.println("This is exceuted after each Test ");
-		System.out.println("This is exceuted after each Test ")
+		System.out.println("This is exceuted after each Test ");
 	}
 
 	@Test

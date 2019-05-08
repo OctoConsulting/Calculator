af751bae7f55014acd5d17ba8968dad9af5bbe2b
nvishy <vishy_work@yahoo.com>
2019-05-08 11:31:03 -0400

    Formatting code CALC-9
    CALC-9

--- src/test/java/com/octo/demo/CalculatorTest.java
+++ src/test/java/com/octo/demo/CalculatorTest.java
@@ -21,7 +21,6 @@ public class CalculatorTest {
 
 	@After
 	public void afterEachTest() {
-		
 		System.out.println("This is executed after each Test ");
 		System.out.println("This is executed after each Test ");
 		System.out.println("This is executed after ech Test ");

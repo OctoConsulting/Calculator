143340030587d04b6756a299725004b1d6e4c130
nvishy <vishy_work@yahoo.com>
2019-05-08 12:46:29 -0400

    CALC-15
    Test add user functionality

--- src/test/java/com/octo/demo/CalculatorTest.java
+++ src/test/java/com/octo/demo/CalculatorTest.java
@@ -21,9 +21,9 @@ public class CalculatorTest {
 
 	@After
 	public void afterEachTest() {
-		System.out.println("This is executed after each Test ");
-		System.out.println("This is executed after each Test ");
-		System.out.println("This is executed after ech Test ");
+		System.out.println("This is executed after each Test");
+		System.out.println("This is executed after each Test");
+		System.out.println("This is executed after ech Test");
 		
 	}
 

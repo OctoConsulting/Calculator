c8aabb180cc8fec98a1f53c41e9605ca398ac2c9
nvishy <vishy_work@yahoo.com>
2019-06-21 15:41:22 -0400

    calc-9
    calc-9

--- src/test/java/com/octo/demo/CalculatorTest.java
+++ src/test/java/com/octo/demo/CalculatorTest.java
@@ -19,6 +19,7 @@ public class CalculatorTest {
 
 	private static DemoCalculator calculator = new DemoCalculator();
 
+	
 	@After
 	public void afterEachTest() {
 		System.out.println("This is executed after each Test");

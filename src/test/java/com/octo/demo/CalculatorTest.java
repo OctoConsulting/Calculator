95c91c4b3b84e478bb9ab6deec649e90eb170bb8
nvishy <vishy_work@yahoo.com>
2019-08-23 16:28:43 -0400

    CALC-21
    CALC-21

--- src/test/java/com/octo/demo/CalculatorTest.java
+++ src/test/java/com/octo/demo/CalculatorTest.java
@@ -18,7 +18,7 @@ public class CalculatorTest {
 	
 	@After
 	public void afterEachTest() {
-		System.out.println("This is executed after each Test");
+		System.out.println("This is executed after each Test")
 	}
 
 	@Test
